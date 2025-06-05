1f71c2acbd40db4f7b1ddc1e6b09c66a1b9620a1
ImmortalIdiot <max.krasulya@yandex.ru>
2025-06-05 20:50:34 +0300

    fix: long error state delay

--- app/src/main/java/com/immortalidiot/weathercompose/ui/utils/Constants.kt
+++ app/src/main/java/com/immortalidiot/weathercompose/ui/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.immortalidiot.weathercompose.ui.utils
 
 object Constants {
     const val WEATHER_QUERY_DURATION = 60_000L
-    const val UI_STATE_DELAY = 3_000L
+    const val UI_STATE_DELAY = 500L
 }
